4976134aabdcd22b9fc536253bbd8204c6d7017a
= <klas_hermansson@hotmail.com>
2024-02-01 14:22:10 +0100

    Initalized HTTP-listener that listens.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,43 @@
 ﻿using real_time_horror_group3;
 using Npgsql;
+using System.Net;
 
 string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=NotSoHomeAlone";
 await using var db = NpgsqlDataSource.Create(dbUri);
 
-await Database.Create(db);
+// await Database.Create(db);
 
+bool listen = true;
 
+Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e)
+{
+    Console.WriteLine("Interupting cancel event");
+    e.Cancel = true;
+    listen = false;
+};
+
+int port = 3000;
+
+HttpListener listener = new();
+listener.Prefixes.Add($"http://localhost:{port}/");
+
+try
+{
+    listener.Start();
+    listener.BeginGetContext(new AsyncCallback(HandleRequest), listener);
+    Console.WriteLine($"Server listening on port {port}");
+    while (listen) { };
+}
+finally
+{
+    listener.Stop();
+}
+
+void HandleRequest(IAsyncResult result)
+{
+    if (result.AsyncState is HttpListener listener)
+    {
+        HttpListenerContext context = listener.EndGetContext(result);
+        listener.BeginGetContext(new AsyncCallback(HandleRequest), listener);
+    }
+}
