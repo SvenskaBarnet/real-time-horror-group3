49ecde2588c8d4c2a20b877d9e6eebad7f2b3df9
= <klas_hermansson@hotmail.com>
2024-02-01 14:03:37 +0100

    Created DB setup
    Created DB-connection
    Created Tables
    Populated DB

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Npgsql;
+
+string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=NotSoHomeAlone";
+await using var db = NpgsqlDataSource.Create(dbUri);
+
+await using var cmd = db.CreateCommand(@"
+CREATE TABLE IF NOT EXISTS room
+(
+    id serial,
+    name text NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS entry_point
+(
+    id serial,
+    name text NOT NULL,
+    room_id integer,
+    is_locked boolean NOT NULL DEFAULT False,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS entry_point
+    ADD CONSTRAINT room_id FOREIGN KEY (room_id) REFERENCES room (id); 
+
+INSERT INTO room(
+	name)
+	VALUES ('Cabin');
+
+INSERT INTO entry_point(
+	name, room_id)
+	VALUES 
+	('Window A', 1), 
+	('Window B', 1),
+	('Door A', 1);
+");
+
+await cmd.ExecuteNonQueryAsync();
+Console.WriteLine("Created and populated DB");
