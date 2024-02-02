c68407e330bcc4130e9b4ab6381dfe20c33dfacb
= <klas_hermansson@hotmail.com>
2024-02-02 11:33:10 +0100

    Breaking door and window check out in seperate class.

---
+++ Check.cs
@@ -0,0 +1,36 @@
+﻿using Npgsql;
+using System.Net;
+using System.Text;
+namespace real_time_horror_group3;
+
+public class Check(NpgsqlDataSource db)
+{
+    public async Task Door(HttpListenerResponse response)
+    {
+        string message = "Here is a door";
+        byte[] buffer = Encoding.UTF8.GetBytes(message);
+        response.ContentType = "text/plain";
+        response.StatusCode = (int)HttpStatusCode.OK;
+
+        response.OutputStream.Write(buffer, 0, buffer.Length);
+        response.OutputStream.Close();
+    }
+
+    public async Task Window(HttpListenerResponse response)
+    {
+        string message = "Here is a window";
+        byte[] buffer = Encoding.UTF8.GetBytes(message);
+        response.ContentType = "text/plain";
+        response.StatusCode = (int)HttpStatusCode.OK;
+
+        response.OutputStream.Write(buffer, 0, buffer.Length);
+        response.OutputStream.Close();
+    }
+
+
+    async Task<string> GetStatus(int roomId, string type)
+    {
+        
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -96,27 +96,6 @@ void NotFound(HttpListenerResponse response)
     response.Close();
 }
 
-void Door(HttpListenerResponse response)
-{
-    string message = "Here is a door";
-    byte[] buffer = Encoding.UTF8.GetBytes(message);
-    response.ContentType = "text/plain";
-    response.StatusCode = (int)HttpStatusCode.OK;
-
-    response.OutputStream.Write(buffer, 0, buffer.Length);
-    response.OutputStream.Close();
-}
-
-void Window(HttpListenerResponse response)
-{
-    string message = "Here is a window";
-    byte[] buffer = Encoding.UTF8.GetBytes(message);
-    response.ContentType = "text/plain";
-    response.StatusCode = (int)HttpStatusCode.OK;
-
-    response.OutputStream.Write(buffer, 0, buffer.Length);
-    response.OutputStream.Close();
-}
 
 void Error(HttpListenerResponse response)
 {
