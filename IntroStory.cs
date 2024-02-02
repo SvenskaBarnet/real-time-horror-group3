3091f005ce1629479ff4a7bd514eec63051fc186
Menaid <menaidnocic@live.se>
2024-02-02 10:13:26 +0100

    added IntroStory class and content

---
+++ IntroStory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace real_time_horror_group3
+{
+    internal class IntroStory
+    {
+    }
+}
