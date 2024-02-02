0b7571bc119cffc66c1a55e0b4fe427dbc8a7468
Menaid <menaidnocic@live.se>
2024-02-02 10:58:54 +0100

    Added Encoding and OutputStream

--- IntroStory.cs
+++ IntroStory.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace real_time_horror_group3
+
+namespace real_time_horror_group3;
+
+    public class IntroStory
 {
-    internal class IntroStory
+    public void CallStory(HttpListenerResponse res)
     {
+        string message = "As a player, I want to wake up in my house by the lake feeling tired and confused, so I can immerse myself in the storyline.";
+
+        byte[] bytes = Encoding.UTF8.GetBytes(message);
+        res.OutputStream.Write(bytes);
+        res.Close();
     }
+
 }
+
