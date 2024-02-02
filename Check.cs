d8479b9d2671ff717d74adf64f582ef5c56c103e
= <klas_hermansson@hotmail.com>
2024-02-02 11:15:28 +0100

    Finished check-function.

--- Check.cs
+++ Check.cs
@@ -10,21 +10,9 @@ public class Check(NpgsqlDataSource db)
         await using (var cmd = db.CreateCommand())
         {
             int roomId = 1;
-            int doorCount = 0;
-            int windowCount = 0;
+            int doorCount = await CountEntryPoints(roomId, "Door");
+            int windowCount = await CountEntryPoints(roomId, "Window");
 
-            await using var findDoors = db.CreateCommand(@"
-                SELECT COUNT(id)
-                FROM entry_point
-	            WHERE room_id = $1 AND type = 'Door';                
-                ");
-            findDoors.Parameters.AddWithValue(roomId);
-            var reader = await findDoors.ExecuteReaderAsync();
-
-            if(await reader.ReadAsync())
-            {
-                doorCount = reader.GetInt32(0);
-            }
 
             string message = $"Room has {doorCount} external doors and {windowCount} windows.";
             byte[] buffer = Encoding.UTF8.GetBytes(message);
@@ -34,6 +22,25 @@ public class Check(NpgsqlDataSource db)
             response.OutputStream.Write(buffer, 0, buffer.Length);
             response.OutputStream.Close();
         }
+    }
+
+    async Task<int> CountEntryPoints(int roomId, string type)
+    {
+        int count = 0;
+            await using var query = db.CreateCommand(@"
+                SELECT COUNT(id)
+                FROM entry_point
+	            WHERE room_id = $1 AND type = $2;                
+                ");
+            query.Parameters.AddWithValue(roomId);
+            query.Parameters.AddWithValue(type);
+            var reader = await query.ExecuteReaderAsync();
+
+            if(await reader.ReadAsync())
+            {
+                count = reader.GetInt32(0);
+            }
+        return count;
     }
 }
 
