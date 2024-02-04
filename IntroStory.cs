0a84e62ec8ec60eb13fbc2e8135f1268fdbced80
= <klas_hermansson@hotmail.com>
2024-02-04 17:21:39 +0100

    Cooler presentation of story

--- IntroStory.cs
+++ IntroStory.cs
@@ -12,11 +12,26 @@ namespace real_time_horror_group3;
 {
     public void CallStory(HttpListenerResponse res)
     {
-        string message = "As a player, I want to wake up in my house by the lake feeling tired and confused, so I can immerse myself in the storyline.";
+        string message = @"
+You wake up in your house by the lake, tired and confused about a sound you hear outside. 
+You are sure that you are home alone and when you go to the window you see two dark figures,
+could this be the burglars that have been all over the news lately? 
+You estimate you have 5 minutes until they reach your house.
+while talking to the police they said they will arrive in 30 minutes to save you.
+Hurry up and secure the home by locking all windows and doors.
+But be aware of your surroundings so you don't kill yourself.
+Check every room in the house carefully.
+Events could happen anytime.
+You need to continue to check the status in every room during your remaining time period.";
 
-        byte[] bytes = Encoding.UTF8.GetBytes(message);
-        res.OutputStream.Write(bytes);
+        foreach (char character in message)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(character.ToString());
+            res.OutputStream.Write(bytes);
+            Thread.Sleep(80);
+        }
         res.Close();
+
     }
 
 }
