eaccbc95f3748815cb9b4adc7bf7fc691d943762
SvenskaBarnet <63249655+SvenskaBarnet@users.noreply.github.com>
2024-02-05 14:36:43 +0100

    Deleted commented code

--- Locking.cs
+++ Locking.cs
@@ -7,20 +7,14 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-
-
 namespace real_time_horror_group3;
-
-
 
 public class Locking(NpgsqlDataSource db)
 {
-
     public async Task Lock(string type, int roomId, HttpListenerRequest req, HttpListenerResponse res)
     {
         StreamReader reader = new(req.InputStream, req.ContentEncoding);
         string lockName = reader.ReadToEnd();
-
        
         await using var cmd = db.CreateCommand(@$"
 
@@ -33,7 +27,6 @@ public class Locking(NpgsqlDataSource db)
         cmd.Parameters.AddWithValue(type);
 
         await cmd.ExecuteNonQueryAsync();
- 
 
         string message = $"{lockName} is now locked";
         byte[] buffer = Encoding.UTF8.GetBytes(message);
@@ -43,72 +36,4 @@ public class Locking(NpgsqlDataSource db)
         res.OutputStream.Write(buffer, 0, buffer.Length);
         res.OutputStream.Close();
     }
-
 }
-
-
-
-/*
-public class Locking
-{
-    public static async Task WindowA()
-    {
-        string dbUri = "Host =localhost;Port=5455;Username=postgres;Password=postgres;Database=NotSoHomeAlone";
-        await using var db = NpgsqlDataSource.Create(dbUri);
-
-        await using var cmd = db.CreateCommand(@"
-                UPDATE entry_point
-                SET is_locked = True
-                WHERE name = 'Window A';
-            ");
-
-        await cmd.ExecuteNonQueryAsync();
-        Console.WriteLine("Window A is now locked.");
-    }
-
-    public static async Task WindowB()
-    {
-        string dbUri = "Host =localhost;Port=5455;Username=postgres;Password=postgres;Database=NotSoHomeAlone";
-        await using var db = NpgsqlDataSource.Create(dbUri);
-
-        await using var cmd = db.CreateCommand(@"
-                UPDATE entry_point
-                SET is_locked = True
-                WHERE name = 'Window B';
-            ");
-
-        await cmd.ExecuteNonQueryAsync();
-        Console.WriteLine("Window B is now locked.");
-    }
-
-    public static async Task DoorA()
-    {
-        string dbUri = "Host =localhost;Port=5455;Username=postgres;Password=postgres;Database=NotSoHomeAlone";
-        await using var db = NpgsqlDataSource.Create(dbUri);
-
-        await using var cmd = db.CreateCommand(@"
-                UPDATE entry_point
-                SET is_locked = True
-                WHERE name = 'Door A';
-            ");
-
-        await cmd.ExecuteNonQueryAsync();
-        Console.WriteLine("Door A is now locked.");
-    }
-}
-
-
-
-
-void Lock(HttpListenerResponse response, string itemName)
-{
-    string message = $"{itemName} is now locked";
-    byte[] buffer = Encoding.UTF8.GetBytes(message);
-    response.ContentType = "text/plain";
-    response.StatusCode = (int)HttpStatusCode.OK;
-
-    response.OutputStream.Write(buffer, 0, buffer.Length);
-    response.OutputStream.Close();
-}
-
-*/
