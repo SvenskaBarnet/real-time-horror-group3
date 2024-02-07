751a2e6dc16a7f471c99cc503584a45a0423017c
= <klas_hermansson@hotmail.com>
2024-02-07 09:11:24 +0100

    Function for moving between rooms

---
+++ Player.cs
@@ -0,0 +1,79 @@
+﻿using Npgsql;
+using System.Net;
+using System.Text;
+namespace real_time_horror_group3;
+
+public class Player(NpgsqlDataSource db)
+{
+    public async Task<string> Create(HttpListenerRequest request, HttpListenerResponse response)
+    {
+        StreamReader reader = new(request.InputStream, request.ContentEncoding);
+        string name = reader.ReadToEnd();
+
+        await using var cmd = db.CreateCommand(@"
+                    INSERT INTO public.player
+                    (name, location)
+                    VALUES($1, 1);
+                    ");
+        cmd.Parameters.AddWithValue(name);
+        await cmd.ExecuteNonQueryAsync();
+
+        string message = $"Player '{name}' has been created";
+        response.StatusCode = (int)HttpStatusCode.Created;
+
+        return message;
+    }
+    public async Task<string> Move(HttpListenerRequest request, HttpListenerResponse response)
+        
+    {
+        StreamReader reader = new(request.InputStream, request.ContentEncoding);
+        string roomName = reader.ReadToEnd();
+        int room = 0;
+        switch (roomName.ToLower())
+        {
+            case "kitchen":
+                room = 1;
+                break;
+            case "hallway":
+                room = 2;
+                break;
+            case "living room":
+                room = 3;
+                break;
+        }
+
+        await using var cmd = db.CreateCommand(@"
+                        UPDATE public.player
+                        SET location = $1
+                        WHERE name = $2;
+                        ");
+        cmd.Parameters.AddWithValue(room);
+        cmd.Parameters.AddWithValue(await Verify(request, response));
+
+        await cmd.ExecuteNonQueryAsync();
+        response.StatusCode = (int)HttpStatusCode.OK;
+        string message = $"Moved to {roomName}";
+
+        return message;
+    }
+    public async Task<string> Verify(HttpListenerRequest request, HttpListenerResponse response)
+    {
+        string? path = request.Url?.AbsolutePath;
+        string? name = path?.Split('/')[1];
+        await Console.Out.WriteLineAsync(name);
+
+        await using var cmd = db.CreateCommand(@"
+            SELECT (name)
+            FROM public.player
+            WHERE name = $1
+            ");
+        cmd.Parameters.AddWithValue(name ?? string.Empty);
+        var reader = await cmd.ExecuteReaderAsync();
+        string username = string.Empty;
+        if(await reader.ReadAsync())
+        {
+            username = reader.GetString(0);
+        }
+        return username;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics.Metrics;
 using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using Npgsql;
 using real_time_horror_group3;
@@ -13,7 +14,7 @@ await database.Create();
 bool listen = true;
 string? message = string.Empty;
 
-Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e) 
+Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e)
 {
     Console.WriteLine("Interuping cancel event");
     e.Cancel = true;
@@ -27,7 +28,7 @@ listener.Prefixes.Add($"http://localhost:{port}/");
 listener.Start();
 Console.WriteLine($"Server listening on port: {port}");
 
-listener.BeginGetContext(new AsyncCallback(Router), listener); 
+listener.BeginGetContext(new AsyncCallback(Router), listener);
 while (listen)
 {
 
@@ -39,26 +40,40 @@ Console.WriteLine("Server stopped");
 
 async void Router(IAsyncResult result)
 {
-    if(result.AsyncState is HttpListener listener)
+    if (result.AsyncState is HttpListener listener)
     {
         HttpListenerContext context = listener.EndGetContext(result);
         HttpListenerResponse response = context.Response;
         HttpListenerRequest request = context.Request;
 
         response.ContentType = "text/plain";
+        Player player = new(db);
 
         switch (request.Url?.AbsolutePath.ToLower())
         {
+            case (string path) when path == "/new-player":
+                if (request.HttpMethod is "POST")
+                {
+                    message = await player.Create(request, response);
+                }
+                break;
+            case (string path) when path == $"/{await player.Verify(request, response)}/move":
+                if (request.HttpMethod is "PATCH")
+                {
+                    message = await player.Move(request, response);
+                }
+                break;
             default:
                 message = "Not Found";
                 response.StatusCode = (int)HttpStatusCode.NotFound;
                 break;
         }
-        
+
         byte[] buffer = Encoding.UTF8.GetBytes(message);
         response.OutputStream.Write(buffer);
         response.OutputStream.Close();
 
         listener.BeginGetContext(new AsyncCallback(Router), listener);
     }
+
 }
