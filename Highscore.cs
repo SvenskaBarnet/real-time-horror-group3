92e59850b12a1ed0fff5eabecd7da4ed2ce065b4
Clone888 <dwclone@gmail.com>
2024-02-13 13:38:42 +0100

    Changed function
    From asyncronic to syncronic

--- Highscore.cs
+++ Highscore.cs
@@ -4,25 +4,25 @@ using System.Net;
 
 namespace real_time_horror_group3;
 
-public class Highscore(NpgsqlDataSource db)
+public class Highscore()
 {
-    public async Task<bool> HandleGameOver(HttpListenerRequest request, HttpListenerResponse response)
+    public static bool HandleGameOver(NpgsqlDataSource db, HttpListenerRequest request, HttpListenerResponse response)
     {
-        Player player = new(db);
-        string playerName = await player.Verify(request, response);
-        await using var cmd = db.CreateCommand(@"
+        string playerName = Player.Verify(db, request, response);
+        var cmd = db.CreateCommand(@"
         SELECT COUNT(*) 
         FROM public.player
         WHERE name = $1 AND is_dead = true;
         ");
         cmd.Parameters.AddWithValue(playerName);
 
-        var reader = await cmd.ExecuteReaderAsync();
+        using var reader = cmd.ExecuteReader();
         int deadPlayer = 0;
-        while (await reader.ReadAsync())
+        while (reader.Read())
         {
             deadPlayer = reader.GetInt32(0);
         }
+        reader.Close();
 
         response.StatusCode = (int)HttpStatusCode.OK;
         if (deadPlayer == 0)
@@ -35,19 +35,17 @@ public class Highscore(NpgsqlDataSource db)
         }
     }
 
-    public async Task AddGameOverTime(HttpListenerRequest request, HttpListenerResponse response)
+    public static void AddScore(NpgsqlDataSource db, HttpListenerRequest request, HttpListenerResponse response)
     {
-        Session session = new(db);
-        Player player = new(db);
-        string playerName = await player.Verify(request, response);
-        var time = session.FormattedTime();
+        string playerName = Player.Verify(db, request, response);
+        var time = Session.FormattedTime(db);
         var highscore = db.CreateCommand(@"
         INSERT INTO public.highscore(player_name, session_time)
         VALUES ($1, $2);
     ");
         highscore.Parameters.AddWithValue(playerName);
         highscore.Parameters.AddWithValue(time);
-        await highscore.ExecuteNonQueryAsync();
+        highscore.ExecuteNonQuery();
     }
 
 
